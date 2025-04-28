cdfe54e69ccca011577ba4d480a9a181d133cf07
Silas Skov Christensen <120374711+Lynsild55@users.noreply.github.com>
2025-04-28 16:36:05 +0200

    Create task_22.sql

---
+++ tasks/3_Basic aggregate functions/task_22.sql
@@ -0,0 +1,51 @@
+select ROUND(COUNT(DISTINCT a1.player_id) / (select COUNT(DISTINCT player_id) from Activity), 2) as fraction
+from Activity a1
+join Activity a2
+on a1.player_id = a2.player_id and DATE_ADD(a1.event_date, INTERVAL 1 DAY) = a2.event_date
+where a1.event_date = 
+        (
+            select MIN(event_date) from Activity where player_id = a1.player_id
+        );
+
+
+/*
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-03-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+
+| fraction  |
++-----------+
+| 0.33      |
++-----------+
+Explanation: 
+Only the player with id 1 logged back in after the first day he had logged in so the answer is 1/3 = 0.33
